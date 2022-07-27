ea4e2dbc49b69be023225cba47871910a4b150c7
Admin <admin@gmail.com>
2022-07-27 20:52:56 +0300

    Add memory leak tacking for components in integration

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -6,7 +6,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
         switch getFeedResult() {
             case let .success(items)?:
-                XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")                
+                XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
                 XCTAssertEqual(items[0], expectedItem(at: 0))
                 XCTAssertEqual(items[1], expectedItem(at: 1))
                 XCTAssertEqual(items[2], expectedItem(at: 2))
@@ -25,10 +25,12 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
         
